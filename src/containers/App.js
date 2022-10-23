7e48471ffa4fd9aa77d3083ff38ad83694b29b91
FlorMerino <florenciamerino012@gmail.com>
2022-10-23 20:51:18 -0300

    add https

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,7 @@ function App() {
   }
   function onSearch(ciudad) {
     //Llamado a la API del clima
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_MYAPIKEY}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${process.env.REACT_APP_MYAPIKEY}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
